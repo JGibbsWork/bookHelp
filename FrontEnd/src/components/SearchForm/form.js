bfd735a5866fe04c4e22635683f73a741f669c06
JGibbsWork <james.gibbs@me.com>
2021-06-24 18:14:54 -0700

    Good Stopping point. Added a bit more styling to Form

--- FrontEnd/src/components/SearchForm/form.js
+++ FrontEnd/src/components/SearchForm/form.js
@@ -19,9 +19,17 @@ export default function Form () {
 
   return (
     <div className="formCard">
+      <h1>Search for something new</h1>
+      <p>Select a city and type in a search term to see how often people are searching for that term. This will produce a comparison on averages from city, state and national search frequencies.</p>
       <form>
+        <label>Select a city</label>
+        <br/>
         <CitySelect onSelect={onSelect}/>
+        <br/>
+        <label>Type your search term</label>
+        <br/>
         <SearchTerm onChange={onChange}/>
+        <br/>
         <SearchIt city={city} search={search}/>
       </form>
     </div>
