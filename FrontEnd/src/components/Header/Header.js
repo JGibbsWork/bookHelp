a606a4ac154fb49009f48808f0a9d288ef9bb981
JGibbsWork <james.gibbs@me.com>
2021-06-25 12:45:31 -0700

    bug fix with header cutting off at different sizes

--- FrontEnd/src/components/Header/Header.js
+++ FrontEnd/src/components/Header/Header.js
@@ -3,25 +3,28 @@ import { Link } from 'react-router-dom';
 
 export default function Header () {
   return (
-    <div className="header">
-      <Link
-      to={{ pathname: '/' }}
-      style={{ textDecoration: "none"}}
-      className="link" >
-        <h3>HOME</h3>
-      </Link>
-      <Link
-      to={{ pathname: '/feed' }}
-      style={{ textDecoration: "none"}}
-      className="link" >
-        <h3>FEED</h3>
-      </Link>
-      <Link
-      to={{ pathname: '/post' }}
-      style={{ textDecoration: "none"}}
-      className="link" >
-        <h3>POST</h3>
-      </Link>
-    </div>
+    <>
+      <div className="header">
+        <Link
+        to={{ pathname: '/' }}
+        style={{ textDecoration: "none"}}
+        className="link" >
+          <h3>HOME</h3>
+        </Link>
+        <Link
+        to={{ pathname: '/feed' }}
+        style={{ textDecoration: "none"}}
+        className="link" >
+          <h3>FEED</h3>
+        </Link>
+        <Link
+        to={{ pathname: '/post' }}
+        style={{ textDecoration: "none"}}
+        className="link" >
+          <h3>POST</h3>
+        </Link>
+      </div>
+      <div className="spacer">&nbsp;</div>
+    </>
   )
 }
